f7051ba9ed3a2ccad8979c2e94677bd3d97e0349
Alpha <georgeofori2005@gmail.com>
2026-07-20 11:09:19 +0000

    July 20, 2026

--- components/body.js
+++ components/body.js
@@ -1,6 +1,6 @@
 const VBody = {
 				template: `
-				<!--
+				
 				<header>
 				<nav class="navbar navbar-default  b-header">
 				<div class="container-fluid">
@@ -18,19 +18,40 @@ const VBody = {
 				</ul>
 				</div>
 				</nav>
-				</header>-->
+				</header>
 				
 				<main>
 				<section class="section-1">
-				<div class=""container-fluid">
-				<div class="hero" id="home">
-				<h1 class="center-block"><span class="np">NovelPixel</span> <span class="st">Studio</span></h1>
-				<span class="shrt-tg center-block">Build Your Online Presence With Us.</span>
-				<p class="center-block">Transforming ideas into beautiful, high-performance websites using modern web technologies.</p>
-				<div class="cta-buttons center-block row">
+				<div class="container-fluid">
+				<div class="bd-hero" id="home">
+				<h1 class="text-center"><span class="np">NovelPixel</span> <span class="st">Studio</span></h1>
+				<span class="shrt-tg text-center">Build Your Online Presence With Us.</span>
+				<p class="text-center">Transforming ideas into beautiful, high-performance websites using modern web technologies.</p>
+				<div class="cta-buttons row">
 				<a class="cta-btn col-xs-12 col-sm-12 col-md-6 col-lg-6" href="#contact">Hire Us</a>
 				<a class="cta-btn col-xs-12 col-sm-12 col-md-6 col-lg-6" href="#projects">View Project</a>
 				</div>
+				</div>
+				
+				<div class="bd-about row" id="about">
+				<div class="bd-abt col-xs-12 col-sm-12 col-md-6 col-lg-6">
+				<div class="bd-prt-1">
+				<span class="bd-label">About</span>
+				<h2 class="bd-h2">Our Mission</h2>
+				<p class="text-center bd-descr">‎Our mission is to design and develop modern, responsive, and user-friendly websites that help businesses and individuals establish a strong online presence. We are committed to delivering high-quality digital solutions that combine creativity, functionality, and performance while ensuring an exceptional user experience.</p>
+				</div>
+				<img alt="mission-picture" src="images/file_00000000869071f486c0944fc4423c54.png" />
+				</div>
+				
+				<div class="bd-abt col-xs-12 col-sm-12 col-md-6 col-lg-6">
+				<div class="bd-prt-1">
+				<span class="bd-label">About</span>
+				<h2 class="bd-h2">Who We Are</h2>
+				<p class="text-center bd-descr">‎‎NovaPixel Studio is a creative web design and frontend development studio dedicated to building modern, responsive, and user-friendly websites. We work with businesses, startups, and individuals to create digital experiences that are visually appealing, easy to use, and tailored to their unique goals.</p>
+				</div>
+				<img alt="who we are picture" src="images/magnific_modern-tech-company-websi_dI0GsEsXSL.png" />
+				</div>
+				
 				</div>
 				
 				</div>
